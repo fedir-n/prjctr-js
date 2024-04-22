60e3d1654456645f6c11cce39dcad9c0ea9308e3
unknown <fedir.nastenko@weblabs.pro>
2024-04-22 19:26:17 +0400

    remove notes

--- HW3/homework3.js
+++ HW3/homework3.js
@@ -17,24 +17,7 @@ console.log(iterativeOddSumTo(1)) // 1
 console.log(iterativeOddSumTo(9)) // 25
 console.log(iterativeOddSumTo(10)) // 25
 
-    // // optimized by chatGPT 
-    // function iterativeOddSumTo(number) { 
-    //     let mathOddSum = 0;   
-    //     let start = number % 2 === 0 ? number - 1 : number; // Початкове число для ітерації
-    //     for (let i = start; i > 0; i -= 2) { // Зменшення на 2 для переходу до наступного непарного числа
-    //         mathOddSum += i;
-    //     }
-    //     return mathOddSum;
-    // } 
-
-    // chatGPT solution
-    // function iterativeOddSumTo(number) {
-    //     let sum = 0;
-    //     for (let i = 1; i <= number; i += 2) {
-    //         sum += i;
-    //     }
-    //     return sum;
-    // }
+   
 
 
 
