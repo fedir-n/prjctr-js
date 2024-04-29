26a153d925abea842b5f9670161b3adafdb657b0
unknown <fedir.nastenko@weblabs.pro>
2024-04-29 15:55:01 +0400

    hw4 ex2

--- HW4/homework4.js
+++ HW4/homework4.js
@@ -9,3 +9,9 @@ const reverseArr = arr.slice().reverse();
 const reverseMaxValue = Number(reverseArr.join(''));
 console.log(reverseMaxValue); // 9854
 console.log(typeof reverseMaxValue); // 'number'
+
+//2. Задача на знаходження добутку масиву чисел з невідомою глибиною вкладеності
+const resultsArray = [1, 2, [3, [4]]];
+const flatArray = resultsArray.flat(Infinity);
+const productOfArray = flatArray.reduce((accumulator, currentValue) => accumulator * currentValue);
+console.log(productOfArray); // 24
