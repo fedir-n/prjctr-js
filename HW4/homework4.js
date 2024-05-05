854b73ae7edd973898eff6b53052956ca2ebeaaa
unknown <fedir.nastenko@weblabs.pro>
2024-05-05 15:15:26 +0400

    fix HW4 ex.1

--- HW4/homework4.js
+++ HW4/homework4.js
@@ -4,9 +4,7 @@
 
 //1. Задача на розворот числа
 const currentMaxValue = 4589;
-const arr = Array.from(String(currentMaxValue), Number);
-const reverseArr = arr.slice().reverse();
-const reverseMaxValue = Number(reverseArr.join(''));
+const reverseMaxValue = parseInt(currentMaxValue.toString().split('').reverse().join(''));
 console.log(reverseMaxValue); // 9854
 console.log(typeof reverseMaxValue); // 'number'
 
