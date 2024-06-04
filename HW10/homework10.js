d91954875ca668ad4730125adc9c10dc359ace15
unknown <fedir.nastenko@weblabs.pro>
2024-06-04 21:02:12 +0400

    change changeTime func

--- HW10/homework10.js
+++ HW10/homework10.js
@@ -43,14 +43,10 @@ document.addEventListener('DOMContentLoaded', () => {
   function changeTime () {
     //{DD-MM-YYYY HH:MM:SS}
     const date = new Date;
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = String(date.getFullYear());
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
-    const lastTurnOnDate = `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
-    
+    const lastTurnOnDate = date
+      .toISOString()
+      .replace('T',' ')
+      .slice(0, -4);    
     isTurnedOn && (localStorage.setItem('lastTurnOnTime', lastTurnOnDate));
   };
 
